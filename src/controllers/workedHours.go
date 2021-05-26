b97d80e884e09a6b4e91f2bf33089e89a3623607
Luiz Lelis <luizhlelis@gmail.com>
2021-05-25 22:58:31 -0300

    chore: making bind avaiable also as method

--- src/controllers/workedHours.go
+++ src/controllers/workedHours.go
@@ -7,10 +7,23 @@ import (
 	"github.com/luizhlelis/gin-playground/models"
 )
 
+func binder(objectToBind interface{}) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if err := c.BindQuery(&objectToBind); err != nil {
+			c.AbortWithStatus(http.StatusBadRequest)
+			return
+		}
+	}
+}
+
 //PostWorkedHours is an API method to an employee register hours
 func PostWorkedHours(ginContext *gin.Context) {
 	var json models.TimeEntry
 
+	// this aproach
+	binder(json)
+
+	// is equivant to this
 	if err := ginContext.ShouldBindJSON(&json); err != nil {
 		ginContext.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
